2570038ddde8c7630048b879ace17aca4f94862c
Joao Moreira <jgmoreira1809@gmail.com>
2022-10-21 19:34:45 -0300

    fixing router... again

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ class App extends React.Component {
     return (
       <Switch>
         <Route exact path="/recipesapp" component={ Login } />
-        <Route exact path="/recipesapp/foods" component={ Recipes } />
-        <Route exact path="/recipesapp/drinks" component={ Recipes } />
-        <Route exact path="/recipesapp/foods/:id" component={ RecipeDetails } />
-        <Route exact path="/recipesapp/drinks/:id" component={ RecipeDetails } />
-        <Route exact path="/recipesapp/profile" component={ Profile } />
+        <Route exact path="/foods" component={ Recipes } />
+        <Route exact path="/drinks" component={ Recipes } />
+        <Route exact path="/foods/:id" component={ RecipeDetails } />
+        <Route exact path="/drinks/:id" component={ RecipeDetails } />
+        <Route exact path="/profile" component={ Profile } />
         <Route
           exact
-          path="/recipesapp/foods/:id/in-progress"
+          path="/foods/:id/in-progress"
           component={ RecipeInProgress }
         />
         <Route
           exact
-          path="/recipesapp/drinks/:id/in-progress"
+          path="/drinks/:id/in-progress"
           component={ RecipeInProgress }
         />
-        <Route exact path="/recipesapp/done-recipes" component={ DoneRecipes } />
-        <Route exact path="/recipesapp/favorite-recipes" component={ FavoriteRecipes } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
       </Switch>
     );
   }
